6adc398800ab1e73efe1bf4da9e7d078040af2e0
Atlanticoc <13886161@qq.com>
2024-08-31 17:22:07 +0800

    增加后台配置项

--- wxapp.wmj.com.cn/public/static/js/admin/Config.js
+++ wxapp.wmj.com.cn/public/static/js/admin/Config.js
@@ -1,79 +1,79 @@
 var CodeInfoDlg = {
-	CodeInfoData: {},
-	deptZtree: null,
-	pNameZtree: null,
-	validateFields: {
-		site_title: {
-			validators: {
-				notEmpty: {
-					message: '站点名称不能为空'
-	 			},
-	 		}
-	 	},
-	 }
+    CodeInfoData: {},
+    deptZtree: null,
+    pNameZtree: null,
+    validateFields: {
+        site_title: {
+            validators: {
+                notEmpty: {
+                    message: '站点名称不能为空'
+                },
+            }
+        },
+    }
 }
 
 
 CodeInfoDlg.clearData = function () {
-	 this.CodeInfoData = {};
+    this.CodeInfoData = {};
 };
 
 
 CodeInfoDlg.set = function (key, val) {
-	 this.CodeInfoData[key] = (typeof value == "undefined") ? $("#" + key).val() : value;
-	 return this;
+    this.CodeInfoData[key] = (typeof value == "undefined") ? $("#" + key).val() : value;
+    return this;
 };
 
 
 CodeInfoDlg.get = function (key) {
-	 return $("#" + key).val();
+    return $("#" + key).val();
 };
 
 
 CodeInfoDlg.close = function () {
-	 var index = parent.layer.getFrameIndex(window.name);
-	 parent.layer.close(index);
+    var index = parent.layer.getFrameIndex(window.name);
+    parent.layer.close(index);
 };
 
 
 CodeInfoDlg.collectData = function () {
-	this.set('').set('yjy_appsecret').set('yjy_appid').set('site_title').set('site_logo').set('keyword').set('description').set('file_size').set('file_type').set('domain').set('copyright').set('wmjappid').set('wmjappsecret').set('wmjaeskey');
+    this.set('').set('yjy_appsecret').set('yjy_appid').set('site_title').set('site_logo').set('keyword').set('description').set('file_size').set('file_type').set('domain').set('copyright').set('wmjappid').set('wmjappsecret').set('wmjaeskey').set('sms_appid').set('sms_appsecret').set('adminpw').set('devicecid').set('autodtkey').set('autodtkeylockid').set('gzhappid').set('gzhappsecret').set('gzhminiappid').set('gzhtempleteid1').set('gzhtempleteid2').set('wxpayappid').set('wxpaykey').set('wxpaymchid').set('wxpaycert_path').set('wxpaykey_path');
 };
 
 
 CodeInfoDlg.index = function () {
-	 this.clearData();
-	 this.collectData();
-	 if (!this.validate()) {
-	 	return;
-	 }
-	 var privacypolicy = UE.getEditor('privacypolicy').getContent();
-	 var serviceagreement = UE.getEditor('serviceagreement').getContent();
-	 var tip = '修改';
-	 var ajax = new $ax(Feng.ctxPath + "/Config/index", function (data) {
-	 	if ('00' === data.status) {
-	 		Feng.success(data.msg);
-	 		CodeInfoDlg.close();
-	 	} else {
-	 		Feng.error(tip + "失败！" + data.msg + "！");
-		 }
-	 })
-	 ajax.set('privacypolicy',privacypolicy);
-	 ajax.set('serviceagreement',serviceagreement);
-	 ajax.set(this.CodeInfoData);
-	 ajax.start();
+    this.clearData();
+    this.collectData();
+    if (!this.validate()) {
+        return;
+    }
+    var privacypolicy = UE.getEditor('privacypolicy').getContent();
+    var serviceagreement = UE.getEditor('serviceagreement').getContent();
+    var tip = '修改';
+    var ajax = new $ax(Feng.ctxPath + "/Config/index", function (data) {
+        if ('00' === data.status) {
+            Feng.success(data.msg);
+            CodeInfoDlg.close();
+        } else {
+            Feng.error(tip + "失败！" + data.msg + "！");
+        }
+    })
+    ajax.set('privacypolicy', privacypolicy);
+    ajax.set('serviceagreement', serviceagreement);
+    ajax.set(this.CodeInfoData);
+    ajax.start();
 };
 
 
 CodeInfoDlg.validate = function () {
-	  $('#CodeInfoForm').data("bootstrapValidator").resetForm();
-	  $('#CodeInfoForm').bootstrapValidator('validate');
-	  return $("#CodeInfoForm").data('bootstrapValidator').isValid();
+    $('#CodeInfoForm').data("bootstrapValidator").resetForm();
+    $('#CodeInfoForm').bootstrapValidator('validate');
+    return $("#CodeInfoForm").data('bootstrapValidator').isValid();
 };
 
 
 $(function () {
-	   Feng.initValidator("CodeInfoForm", CodeInfoDlg.validateFields);
+    Feng.initValidator("CodeInfoForm", CodeInfoDlg.validateFields);
 });
 
 
