c6ffe18a7b0a9373d79fff1f42a7eeb923c18283
Duan <13886161@qq.com>
2026-06-07 00:20:01 +0800

    恢复扫码登录原有二维码规则

--- weimenjin_admin/config/my.php
+++ weimenjin_admin/config/my.php
@@ -136,7 +136,7 @@ return [
         'default_audio_url' => Env::get('live_talk.default_audio_url', '/audio/wmj.mp3'),
     ],
     'update' => [
-        'current_version' => Env::get('update.current_version', '2026.06.06.12'),
+        'current_version' => Env::get('update.current_version', '2026.06.06.13'),
         'manifest_url' => Env::get('update.manifest_url', 'https://demo.wmj.com.cn/updates/manifest.json'),
     ],
     'login' => [
